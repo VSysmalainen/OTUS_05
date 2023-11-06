c5268edc062aa581332993334b95f2005db6833c
VSysmalainen <sysmalainenviktor@gmail.com>
2023-11-06 17:00:45 +0300

    commit 1.2

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import csv
 import json
-from files import CSV_FILE_PATH
-from files import JSON_FILE_PATH
 from collections import OrderedDict
 from math import ceil
+from files import CSV_FILE_PATH
+from files import JSON_FILE_PATH
 
 
 with open(CSV_FILE_PATH, 'r', newline='') as c:
@@ -18,15 +18,15 @@ with open(JSON_FILE_PATH, 'r') as j:
     part_len = ceil(len(all_books) / len(json_data))
     parts_of_books = [all_books[part_len * k:part_len * (k + 1)] for k in range(len(json_data))]
 
-    s = []
+    result = []
     for i in json_data:
-        d = {k[0]: k[1] for k in i.items() if k[0] in ('name', 'gender', 'address', 'age')}
-        od = OrderedDict(d)
+        users_cut_dict = {k[0]: k[1] for k in i.items() if k[0] in ('name', 'gender', 'address', 'age')}
+        od = OrderedDict(users_cut_dict)
         od.move_to_end('age')
-        b = dict(od)
-        b.update({'books': parts_of_books[0]})
+        users_cut_dict_sort = dict(od)
+        users_cut_dict_sort.update({'books': parts_of_books[0]})
         parts_of_books.pop(0)
-        s.append(b)
+        result.append(users_cut_dict_sort)
 
 with open('result.json', 'w') as file:
-    json.dump(s, file, indent=4)
+    json.dump(result, file, indent=4)
